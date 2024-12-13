e8551e79cb8cae75a915e5176f53ce4affa326f1
MartasDemon <matomihalik223@gmail.com>
2024-12-13 13:32:30 +0100

    prcam toto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from "next";
+"use client";
+
 import "./globals.css";
 import SimpleBottomNavigation from "../components/NavBar";
-import AuthProvider from "../components/AuthProvider"; 
+import AuthProvider from "../components/AuthProvider";
+import ThemeProvider from "../components/ThemeProvider";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "SnapZoska",
-  description: "Created by student Martin Mihalik",
+  description: "Created by student Martin Mihálik",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="sk">
       <body>
-        <AuthProvider>
-          {children}
-          <SimpleBottomNavigation /> {}
-        </AuthProvider>
+        <ThemeProvider>
+          <AuthProvider>
+            {children}
+            <SimpleBottomNavigation />
+          </AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
