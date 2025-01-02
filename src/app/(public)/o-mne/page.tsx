7eca2e9f58a93f49c4239ba0ef38a00bff987ee7
MartasDemon <matomihalik223@gmail.com>
2025-01-02 15:33:05 +0100

    commit

--- src/app/(public)/o-mne/page.tsx
+++ src/app/(public)/o-mne/page.tsx
@@ -1,14 +1,11 @@
 // src/app/o-mne/page.tsx
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 
 export const metadata = { title: "O-mne| ZoskaSnap"}
 
 export default function AboutMe() {
 
   return (
-    <Container>
-        <Typography> O-mne stránka </Typography>
-    </Container>
+        <Typography>O-mne stránka</Typography>
   );
 }
--- src/sections/SignInView.tsx
+++ src/sections/SignInView.tsx
@@ -1,14 +1,13 @@
-// src/sections/SignInView.tsx
-
 "use client";
 
 import {
-    Button,
-    Container,
-    Typography,
-  } from "@mui/material";
-  import { signIn } from "next-auth/react";
-  import GoogleIcon from "@mui/icons-material/Google"
+  Button,
+  Container,
+  Typography,
+  Link,
+} from "@mui/material";
+import { signIn } from "next-auth/react";
+import GoogleIcon from "@mui/icons-material/Google";
 
 export default function SignInView() {
   return (
@@ -25,18 +24,34 @@ export default function SignInView() {
         borderRadius: 2,
       }}
     >
-      {/* Logo / Title */}
       <Typography variant="h5" sx={{ mb: 3 }}>
         Prihlásenie
       </Typography>
+      
+      <Typography variant="body2" sx={{ mt: 2, textAlign: "center" }}>
+        Ešte nemáte účet?{" "}
+        <Link
+          href="/auth/registracia"
+          sx={{ color: "blue", fontWeight: "bold" }}
+        >
+          Registrujte sa
+        </Link>
+      </Typography>
 
-      {/* Google Sign Up */}
       <Button
         variant="outlined"
         fullWidth
         startIcon={<GoogleIcon />}
         onClick={() => signIn("google")}
-        sx={{ mb: 1 }}
+        sx={{
+          mb: 1,
+          color: "red",
+          borderColor: "red",
+          "&:hover": {
+            backgroundColor: "rgba(255, 0, 0, 0.1)",
+            borderColor: "red",
+          },
+        }}
       >
         Prihlásiť sa účtom Google
       </Button>
